3d37b3ded6842d4ac57be5855a9bb8ffdf17445e
SAYAN DAS <poke19962008@gmail.com>
2017-03-12 23:52:04 +0530

    added assets and readme

---
+++ README.md
@@ -0,0 +1,49 @@
+# Today's Extension
+
+Simple easy to use iOS app extension, to have a quick look over your attendance. Tested over iPhone 7 running on iOS 10.3(beta) [requires Xcode 8.3 beta to run]. Thanks to [A2O](https://github.com/NihalSh/a2o) for providing the API.
+
+> This has been developed keeping user's privacy in mind. 
+> No user credentials are saved on cloud.
+
+Extension Screen           |  App Screen
+:-------------------------:|:-------------------------:
+![](https://...Ocean.png)  |  ![](https://...Dark.png)
+
+### Run Server
+
+```bash
+$ git clone https://github.com/NihalSh/a2o.git
+$ cd a20
+$ npm i | node app.js # Change the host to external IP if needed
+```
+
+The MIT License (MIT)
+=====================
+
+Copyright © `2017` `SAYAN DAS`
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the “Software”), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+
+
--- Today Attendance/TodayViewController.swift
+++ Today Attendance/TodayViewController.swift
@@ -13,16 +13,15 @@ class TodayViewController: UIViewController, NCWidgetProviding, UICollectionView
     
 //    Global Configs
     let timerInterval: Double = 3*60*60
-    let urlRoot: String = "http://139.59.3.76:60000"
+    let urlRoot: String = "http://192.168.1.3:60000" // Bind with external IP
     var accessToken: String = "<ACCESS-TOKEN>"
     
     var timer: Timer?
     let defaults = UserDefaults.standard
     let groupDefaults = UserDefaults.init(suiteName: "group.TodayAccessTokenDefault")
     let reuseIdentifier = "Cell"
-    var items: [Dictionary<String, String>] = []
-    
-    let subMap: [String:String] = [
+    var items = [Array<String>]()
+    let subMap: [String:String] = [  // To avoid text overflow
         "IC1053": "Control System",
         "MA1006A": "Maths",
         "CS1014": "Compiler",
@@ -40,9 +39,11 @@ class TodayViewController: UIViewController, NCWidgetProviding, UICollectionView
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
         
+        // If user has logged in from app
         if(groupDefaults?.string(forKey: "AccessToken") != nil) {
             accessToken = (groupDefaults?.string(forKey: "AccessToken"))!
-        
+            
+            // Expand view and check data existence
             self.extensionContext?.widgetLargestAvailableDisplayMode = NCWidgetDisplayMode.expanded
             if(self.items.count == 0) {
                 CollectionView.isHidden = true
@@ -52,6 +53,7 @@ class TodayViewController: UIViewController, NCWidgetProviding, UICollectionView
                 loader.isHidden = true
             }
             
+            // Refresh at every timerInterval
             timer = Timer.scheduledTimer(timeInterval: timerInterval, target: self, selector: #selector(TodayViewController.fetchDetails), userInfo: nil, repeats: true)
             self.fetchDetails()
         }else {
@@ -60,6 +62,7 @@ class TodayViewController: UIViewController, NCWidgetProviding, UICollectionView
         }
     }
     
+    // Adjust extension height
     func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
         if (activeDisplayMode == NCWidgetDisplayMode.compact) {
             self.preferredContentSize = maxSize
@@ -100,20 +103,20 @@ class TodayViewController: UIViewController, NCWidgetProviding, UICollectionView
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath)
         
         let subCode = cell.viewWithTag(3) as! UILabel
-        subCode.text =  items[indexPath.row]["Course Code"]! as String
+        subCode.text =  items[indexPath.row][0] as String
         subCode.text = String(subCode.text!.characters.dropLast(7))
         
         let subName = cell.viewWithTag(2) as! UILabel
-        subName.text = (self.subMap[subCode.text!] != nil) ? self.subMap[subCode.text!]!: items[indexPath.row]["Course Title"]!
+        subName.text = (self.subMap[subCode.text!] != nil) ? self.subMap[subCode.text!]!: items[indexPath.row][1]
         
         let attn = cell.viewWithTag(1) as! UILabel
-        attn.text = (items[indexPath.row]["Attn %"]! as String) + "%"
+        attn.text = (items[indexPath.row][8] as String) + "%"
         
         return cell
     }
     
     
-    
+    // API Call Method
     func fetchDetails() {
         let url = URL(string: urlRoot+"/attendance/"+accessToken)
         
@@ -123,12 +126,14 @@ class TodayViewController: UIViewController, NCWidgetProviding, UICollectionView
         
     }
     
+    // API Call CallBack
     func fetchDetailsCB(data: Data?, resp: URLResponse?, error: Error?) -> Void {
         if (error == nil) {
             do {
                 let data = try JSONSerialization.jsonObject(with: data!, options:[])
                 DispatchQueue.main.async() {
-                    self.items = data as! [Dictionary<String, String>]
+                    self.items = data as! [Array<String>]
+                    self.items.remove(at: 0) // First element is irrelevant
                     self.loader.isHidden = true
                     self.CollectionView.isHidden = false
                     self.CollectionView.reloadData()
@@ -138,7 +143,7 @@ class TodayViewController: UIViewController, NCWidgetProviding, UICollectionView
                     let time = format.string(from: Date())
                     self.updateTime.text = "Last updated: " + time + " | RefreshCycles: 3Hr."
                     
-                    self.saveDefaults(data: data as! [Dictionary<String, String>], time: time )
+                    self.saveDefaults(data: self.items, time: time )
                     
                     print("Item Loaded." + " | Time: "+time)
                 }
@@ -163,13 +168,13 @@ class TodayViewController: UIViewController, NCWidgetProviding, UICollectionView
         }
     }
     
-    func saveDefaults(data: [Dictionary<String, String>], time: String) {
+    func saveDefaults(data: [Array<String>], time: String) {
         defaults.set(data, forKey: "attendance")
         defaults.setValue(time, forKey: "lastUpdate")
     }
     
-    func fetchDefaults() -> ([Dictionary<String, String>], String) {
-        let attn = defaults.array(forKey: "attendance") as! [Dictionary<String, String>]
+    func fetchDefaults() -> ([Array<String>], String) {
+        let attn = defaults.array(forKey: "attendance") as! [Array<String>]
         let lUpd = defaults.string(forKey: "lastUpdate")
         
         return (attn, lUpd!)
